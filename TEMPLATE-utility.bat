2ed94278aaa36d64fa77764af08b027304476002
TAFEITLAB <tafewollongbarsys@gmail.com>
2022-07-18 11:37:14 +1000

    Deleted

--- TEMPLATE-utility.bat
+++ TEMPLATE-utility.bat
@@ -1,77 +1,77 @@
 @echo off
 
-REM | =============================================
-REM | NAME
-REM | =============================================
-REM | Purpose: 
-REM | Requirements:
-REM | Author: 
-REM | Organisation: TAFEITLAB
+:: =============================================
+:: NAME
+:: =============================================
+:: Purpose: 
+:: Requirements:
+:: Author: 
+:: Organisation: 
 
-REM | ================
-REM | VERSION HISTORY
-REM | ================ 
-REM | -- YYYY-MM-DD --
-REM | 
-REM | ----------------
+:: ================
+:: VERSION HISTORY
+:: ================ 
+:: -- YYYY-MM-DD --
+:: 
+:: ----------------
 
-REM | ===============================
-REM | PREPARE ENVIRONMENT FOR SCRIPT
-REM | ===============================
+:: ===============================
+:: PREPARE ENVIRONMENT FOR SCRIPT
+:: ===============================
 
-REM | Set location of batch file as current working directory
-REM | NB: Breakdown of %~dp0:
-REM |     % = start variable
-REM |     ~ = remove surrounding quotes
-REM |     0 = filepath of script
-REM |     d = Expand 0 to drive letter only
-REM |     p = expand 0 to path only
-REM |     Therefore %~dp0 = 
-REM |        Get current filepath of script (drive letter and path only)
-REM |        No quote marks.
+:: Set location of batch file as current working directory
+:: NB: Breakdown of %~dp0:
+::     % = start variable
+::     ~ = remove surrounding quotes
+::     0 = filepath of script
+::     d = Expand 0 to drive letter only
+::     p = expand 0 to path only
+::     Therefore %~dp0 = 
+::        Get current filepath of script (drive letter and path only)
+::        No quote marks.
 pushd "%~dp0"
 
-REM | Clear screen.
+:: Clear screen.
 cls
 
-REM | Get date info in ISO 8601 standard date format (yyyy-mm-dd)
-REM | NB: The SET function below works as follows:
-REM |     VARIABLENAME:~STARTPOSITION,NUMBEROFCHARACTERS
-REM |     Therefore in the string "20190327082654.880000+660"
-REM |     ~0,4 translates to 2019
-REM | NB: Carat (Escape character "^") needed to ensure pipe is processed as part of WMIC command instead of as part of the "for" loop
+:: Get date info in ISO 8601 standard date format (yyyy-mm-dd)
+:: NB: The SET function below works as follows:
+::     VARIABLENAME:~STARTPOSITION,NUMBEROFCHARACTERS
+::     Therefore in the string "20190327082654.880000+660"
+::     ~0,4 translates to 2019
+:: NB: Carat (Escape character "^") needed to ensure pipe is processed as part of WMIC command instead of as part of the "for" loop
 for /f %%a in ('WMIC OS GET LocalDateTime ^| find "."') do set DTS=%%a
 set LOGDATE=%DTS:~0,4%-%DTS:~4,2%-%DTS:~6,2%
 set LOGTIME=%DTS:~8,2%:%DTS:~10,2%:%DTS:~12,2%
 
-REM | Logfile
-REM | - Set log directory
+:: Logfile
+:: - Set log directory
 set LOGPATH=%SYSTEMDRIVE%\Logs
-REM | NB: %0 = Full file path of script
-REM |     %~n0% = Script file name, no file extension
-REM |     %~n0~x0 = Script file name, with file extension
-REM | - Set log file name
-REM |   - Include log path to ensure it is saved in the correct location.
+:: NB: %0 = Full file path of script
+::     %~n0% = Script file name, no file extension
+::     %~n0~x0 = Script file name, with file extension
+:: - Set log file name
+::   - Include log path to ensure it is saved in the correct location.
 set LOGFILE=
 
-REM | Create log directory if does not exist
+:: Create log directory if does not exist
 IF NOT EXIST %LOGPATH% MKDIR %LOGPATH%
 
-REM | Log location and name. Do not use trailing slashes (\)
+:: Log location and name. Do not use trailing slashes (\)
 set LOGPATH=%SystemDrive%\Logs
 set LOGFILE=
 
-REM | ======
-REM | TASKS
-REM | ======
+:: ======
+:: TASKS
+:: ======
 
 
-REM | ============================
-REM | CLEAR ENVIRONMENT and EXIT
-REM | ============================
+:: ============================
+:: CLEAR ENVIRONMENT and EXIT
+:: ============================
 
-REM | Reset current working directory
+:: Reset current working directory
 popd
 
-REM | Exit and return exit code to PDQ Deploy
+:: Exit and return exit code to PDQ Deploy
 exit /B %EXIT_CODE%
